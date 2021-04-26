f1db50d37c10d631cdce1aea0a7cae17bd292c17
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-26 15:22:44 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,11 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  book_trilogies = {
+    "Tolkien" => "Lord of the Rings",
+    "Carey" => "The Rampart Trilogy",
+    "Pullman" => "His Dark Materials",
+    "Collins" => "The Hunger Games Trilogy"
+  }
 end
 
 
@@ -9,9 +13,14 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
+  return the_manifest
 end
+
 
 def retrieval
   shipping_manifest = {
@@ -21,8 +30,9 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
+
 
 def adding
   shipping_manifest = {
@@ -31,13 +41,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
-
+  return shipping_manifest
 end
